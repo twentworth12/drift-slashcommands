7cf9fe4fe5e877be505f8ae8443d8a2fc563a059
twentworth12 <twentworth12@gmail.com>
2018-02-27 21:57:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -61,15 +61,6 @@ function postMessage(body, conversationId, orgId) {
     'orgId': orgId,
     'body': body,
     'type': 'private_prompt',
-    'buttons': [{
-      'label': 'Send',
-      'value': 'hello world',
-      'type': 'reply',
-      'style': 'primary',
-      'reaction': {
-        'type': 'delete'
-      	}
-    	},]
      }
     
     // Send the message
