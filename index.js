02dac8bd18f279f0a3abf858cacda18fba5d0fc5
twentworth12 <twentworth12@gmail.com>
2018-02-28 14:19:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.listen(process.env.PORT || 3000, () => console.log('googlethat listening on 
 app.post('/api', (req, res) => {
   
   if (req.body.type === 'new_message') {
-      console.log("New Drift message!!!")
+      console.log("New Drift message!!! " + req.body.data.body)
       handleMessage(req.body.orgId, req.body.data);  
   }
   return res.send('ok')
