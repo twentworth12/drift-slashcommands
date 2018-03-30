cdc730a644abd4949018d818cec779de93ea9abf
twentworth12 <twentworth12@gmail.com>
2018-03-30 16:51:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function memeThat (conversationId, orgId, messageBody) {
 	   request
 	  .get("https://api.imgflip.com/caption_image?template_id=14859329&username=IMGFLIP_USER&pass=IMGFLIP_PASS&text0=" + messageBody)
 	  .end(function (err, res) {
-		var meme = "<a href=" + res.data.url + ">"
+		var meme = "<a href=" + res.url + ">"
 		console.log("meme is " + meme);
 		postMessage(meme, conversationId, orgId)
 		return
