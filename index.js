aa2e799fed324d997edbf42e6b0d93940024752f
twentworth12 <twentworth12@gmail.com>
2018-08-22 15:29:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function woodwardThat (conversationId, orgId, messageBody) {
 	    
     const driftMessage = {
     'orgId': orgId,
-    'body': "<p><img src='https://img.buzzfeed.com/buzzfeed-static/static/2017-02/10/16/enhanced/buzzfeed-prod-fastlane-03/original-grid-image-10291-1486761196-4.jpg'><br/>https://www.youtube.com/watch?v=QyS_o6lqKS0",
+    'body': "<p><img src='https://img.buzzfeed.com/buzzfeed-static/static/2017-02/10/16/enhanced/buzzfeed-prod-fastlane-03/original-grid-image-10291-1486761196-4.jpg'><br/><a href='https://www.youtube.com/watch?v=QyS_o6lqKS0'>Hi Dave Woodward</a>",
     'type': 'private_prompt',
     }	
 	
