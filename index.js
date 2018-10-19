23bb1de12026632074304eb4481c843431ad9c9a
twentworth12 <twentworth12@gmail.com>
2018-10-19 16:24:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function communityPost (conversationId, orgId, messageBody) {
     .send(forumMessage)
     .end(function (err, res) {
 	console.log("Posted to Vanilla");
-	var returnmessage="I created a post in the community for you<br/>" + res.url
+	var returnmessage="I created a post in the community for you<br/>" + res.body.url
 	return postMessage(returnmessage, conversationId, orgId);
 	});
 }
